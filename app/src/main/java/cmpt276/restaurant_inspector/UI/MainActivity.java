ebba0d174d1f16ebf23be4922620f1767523cd2a
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2020-03-18 10:19:14 -0700

    Creat mapsActivity

--- app/src/main/java/cmpt276/restaurant_inspector/UI/MainActivity.java
+++ app/src/main/java/cmpt276/restaurant_inspector/UI/MainActivity.java
@@ -2,9 +2,15 @@ package cmpt276.restaurant_inspector.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 /**
  *  Splash screen to welcome user
@@ -12,6 +18,8 @@ import android.os.Handler;
 public class MainActivity extends AppCompatActivity
 {
     Runnable runnable;
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,15 +28,36 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Handler handler = new Handler();
 
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-            Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
-            startActivity(intent);
-            finish();
-            }
-        };
+        if(isServiceOK()) {
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
 
-        handler.postDelayed(runnable, 3000);
+            handler.postDelayed(runnable, 3000);
+        }
+    }
+    public boolean isServiceOK(){
+        Log.d(TAG,"isServiceOK: checking google service version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and user can make map requests
+            Log.d(TAG, "isServiceOK: Google Map working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG,"isServiceOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"You cant make map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
