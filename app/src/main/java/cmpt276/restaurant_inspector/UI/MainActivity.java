0d3edc35cc7eae35126f4723c331e9a503cac4df
Jordan Chong <jtchong@sfu.ca>
2020-03-27 05:39:19 -0700

    data downloading crashes, removed for time being

--- app/src/main/java/cmpt276/restaurant_inspector/UI/MainActivity.java
+++ app/src/main/java/cmpt276/restaurant_inspector/UI/MainActivity.java
@@ -9,7 +9,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import cmpt276.restaurant_inspector.service.FileDownloadClient;
 import okhttp3.ResponseBody;
@@ -59,19 +66,18 @@ public class MainActivity extends AppCompatActivity
     private void downloadRestaurants() {
         //create Retrofit instance
         Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl("https://futurestud.io/");
+                .baseUrl("http://data.surrey.ca/api/3/action/package_show?id=restaurants/");
 
         Retrofit retrofit = builder.build();
 
-        //todo get client & call object for the request
         FileDownloadClient fileDownloadClient = retrofit.create(FileDownloadClient.class);
-
         Call<ResponseBody> call = fileDownloadClient.downloadRestaurants();
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                boolean writtenToDisk = writeResponseBodyToDisk(response.body());
             }
 
             @Override
@@ -80,4 +86,55 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
+
+    private boolean writeResponseBodyToDisk(ResponseBody body) {
+        try {
+            //change pathname to proper file directory
+            File restaurants = new File(getExternalFilesDir(null) + File.separator + "restaurants.csv");
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(restaurants);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    Log.d("Restaurant file", "file download: " + fileSizeDownloaded + " of " + fileSize);
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
 }
