1706af3a9e95649ea6191a8b5b2abf90f99e2ef8
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2020-03-27 02:01:52 -0700

    Refactor variable'name

--- app/src/main/java/cmpt276/restaurant_inspector/MapsClass/MyItem.java
+++ app/src/main/java/cmpt276/restaurant_inspector/MapsClass/MyItem.java
@@ -1,7 +1,5 @@
 package cmpt276.restaurant_inspector.MapsClass;
 
-import android.graphics.drawable.Icon;
-
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
@@ -9,17 +7,17 @@ import com.google.maps.android.clustering.ClusterItem;
 // This class handle each restaurant marker in Google Map
 
 public class MyItem implements ClusterItem {
-    private final LatLng mPosition;
-    private String mTitle = "";
-    private String mSnippet = "";
+    private final LatLng position;
+    private String title = "";
+    private String snippet = "";
     private final BitmapDescriptor icon;
     private final int positionInRestaurantList;
     private final String harzardLevel;
 
-    public MyItem(double lat, double lng, String mTitle, String mSnippet, BitmapDescriptor icon, int positionInRestaurantList, String harzardLevel) {
-        this.mPosition = new LatLng(lat, lng);
-        this.mTitle = mTitle;
-        this.mSnippet = mSnippet;
+    public MyItem(double lat, double lng, String title, String snippet, BitmapDescriptor icon, int positionInRestaurantList, String harzardLevel) {
+        this.position = new LatLng(lat, lng);
+        this.title = title;
+        this.snippet = snippet;
         this.icon = icon;
         this.positionInRestaurantList = positionInRestaurantList;
         this.harzardLevel = harzardLevel;
@@ -27,16 +25,16 @@ public class MyItem implements ClusterItem {
 
     @Override
     public LatLng getPosition() {
-        return mPosition;
+        return position;
     }
 
     @Override
     public String getTitle() {
-        return mTitle;
+        return title;
     }
     @Override
     public String getSnippet() {
-        return mSnippet;
+        return snippet;
     }
 
     public BitmapDescriptor getIcon() {
