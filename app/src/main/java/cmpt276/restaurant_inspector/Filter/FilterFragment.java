a6e05fcb21003cf40304340443603c8c114367f0
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2020-04-02 15:41:08 -0700

    Error restaurant activity

--- app/src/main/java/cmpt276/restaurant_inspector/Filter/FilterFragment.java
+++ app/src/main/java/cmpt276/restaurant_inspector/Filter/FilterFragment.java
@@ -31,47 +31,41 @@ public class FilterFragment extends AppCompatDialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
         final Boolean[] isFavouriteList = new Boolean[1];
+        isFavouriteList[0] = false;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.filter_dialog, null);
         isFavourite = (Switch) view.findViewById(R.id.switch_filter_box_isFavourite);
         hazardLevel = (Spinner) view.findViewById(R.id.spinner_filter_box_hazard_level);
         numberOfViolationsMoreThanEditText = (EditText) view.findViewById(R.id.editText_filter_box_number_of_violations);
 
-        isFavourite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                isFavouriteList[0] = isChecked;
-            }
-        });
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.hazard_level, R.layout.support_simple_spinner_dropdown_item);
+        isFavourite.setOnCheckedChangeListener((compoundButton, isChecked) -> isFavouriteList[0] = isChecked);
 
-        //adapter.setDropDownViewResource(R.layout.filter_dialog);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.hazard_level, R.layout.support_simple_spinner_dropdown_item);
+        hazardLevel.setPrompt("Select harzard level...");
         hazardLevel.setAdapter(adapter);
 
         builder.setView(view)
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
+                .setNegativeButton("Cancel", (dialogInterface, i) -> {
+                    Log.i("isChecked",String.valueOf(isFavouriteList[0]));
+                    Log.i("hazard level",hazardLevel.getSelectedItem().toString());
                 })
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.i("isChecked",String.valueOf(isFavouriteList[0]));
-                        Log.i("hazard level",hazardLevel.getSelectedItem().toString());
-                        String numberOfViolationsMoreThanString = numberOfViolationsMoreThanEditText.getText().toString();
-                        int numberOfViolationsLessThan;
-                                if("".equals(numberOfViolationsMoreThanString)){
-                                    numberOfViolationsLessThan = 0;
-                                }
-                                else {
-                                    numberOfViolationsLessThan = Integer.parseInt(numberOfViolationsMoreThanString);
-                                }
-                        listener.getInput(isFavouriteList[0],hazardLevel.getSelectedItem().toString(),numberOfViolationsLessThan);
+                .setPositiveButton("OK", (dialogInterface, i) -> {
+                    Log.i("isChecked",String.valueOf(isFavouriteList[0]));
+                    Log.i("hazard level",hazardLevel.getSelectedItem().toString());
+                    String numberOfViolationsMoreThanString = numberOfViolationsMoreThanEditText.getText().toString();
+                    int numberOfViolationsLessThan;
+
+                    if ("".equals(numberOfViolationsMoreThanString)){
+                        numberOfViolationsLessThan = 0;
                     }
+                    else {
+                        numberOfViolationsLessThan = Integer.parseInt(numberOfViolationsMoreThanString);
+                    }
+
+                    listener.getInput(isFavouriteList[0],hazardLevel.getSelectedItem().toString(),numberOfViolationsLessThan);
                 });
+
         return builder.create();
 
     }
